4cef7352428148feb246bb83c13febd3e7293a41
Andrew Kharlamov <kharlamov@gmail.com>
2012-09-03 19:39:05 -0600

    logout redirection

---
+++ Bottles/Blog.Authorization/Content/scripts/logout.js
@@ -0,0 +1,15 @@
+﻿define('logout', ['jquery'], function ($) {
+  var count = 5,
+    counter = $('#count'),
+    id = setInterval(function () {
+      if (count <= 0) {
+        clearInterval(id);
+        window.location = "/";
+      }
+
+      count--;
+      counter.text(count);
+    }, 1000);
+
+  $('.current').hide();
+});

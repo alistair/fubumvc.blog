66ea5404c8748f7fcb1f4ef204d91dafe893a41a
Andrew Kharlamov <kharlamov@gmail.com>
2012-09-24 21:13:56 -0600

    started initial client side paging (currently comments only)

---
+++ Blog/Content/scripts/util/require.query-string.js
@@ -0,0 +1,27 @@
+﻿define('query-string', [], function () {
+    var query = { },
+        match,
+        queryString = window.location.search.substring(1),
+        replacePlusWithSpaceRegex = /\+/g,
+        regex = /([^&=]+)=?([^&]*)/g,
+        decodeUri = function (string) {
+            return decodeURIComponent(string.replace(replacePlusWithSpaceRegex, ' '));
+        };
+
+    query.getParameterByName = function (name) {
+        match = RegExp(name + '=([^&]*)').exec(queryString);
+
+        return match && decodeUri(match[1]);
+    };
+
+    query.getParameters = function () {
+        var params = { };
+        while (match = regex.exec(queryString)) {
+            params[decodeUri(match[1])] = decodeUri(match[2]);
+        }
+
+        return params;
+    };
+
+    return query;
+});
--- Bottles/Blog.Comments/Content/scripts/managecomments.js
+++ Bottles/Blog.Comments/Content/scripts/managecomments.js
@@ -1,4 +1,25 @@
-﻿define('managecomments', ['jquery'], function ($) {
+﻿define('managecomments', ['jquery', 'query-string'], function ($, qs) {
+  var countInput = $('input[name="count"]'),
+      pageInfo = function () {
+        var data = qs.getParameters();
+
+        return {
+          count: data.Count ? parseInt(data.Count, 10) : 25,
+          page: data.Page ? parseInt(data.Page, 10) : 1
+        };
+      } (),
+      setPaging = function () {
+        $('.nextpage').attr('href', '?Count=' + pageInfo.count + '&Page=' + (pageInfo.page + 1));
+
+        if (pageInfo.count !== 1) {
+          $('.prevpage').attr('href', '?Count=' + pageInfo.count + '&Page=' + (pageInfo.page - 1));
+        }
+
+          $('.firstpage').attr('href', '?Count=' + pageInfo.count + '&Page=1');
+
+        countInput.val(pageInfo.page);
+      };
+
   $('.delete-comment').click(function () {
     var link = $(this),
       data = link.data();
@@ -12,4 +33,14 @@
       }
     });
   });
+
+  setPaging();
+
+  countInput.blur(function () {
+    var val = parseInt(countInput.val(), 10);
+    if (val !== pageInfo.count) {
+      window.location = '?Count=' + pageInfo.count + '&Page=' + val;
+    }
+  });
+
 });
